5a17375d4c257be3672e8919ebcaddf60e7f2fe8
mcfinley <mcfinley@yandex.ru>
2020-06-29 13:55:44 +0300

    Start from starter

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import './index.html'
+
+import HtmlBasement from './html-basement'
+import QuintusElements from './quintus-elements'
+import MainRender from './main-render'
+// import CellsEngine from './cells-engine'
+import AdvancedEvents from './advanced-events'
+// import GameMode from './game-mode'
+import MapNavigation from './map-navigation'
+
+// import BottomInterface from './bottom-interface'
+// import InstrumentsPalette from './instruments-palette'
+
+// import GameActions from './game-actions'
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  const root = document.getElementById('app')
+
+  const html = new HtmlBasement(root)
+  const elements = new QuintusElements()
+
+  // const engine = new CellsEngine(state)
+  const events = new AdvancedEvents(html)
+  // const mode = new GameMode()
+  const map = new MapNavigation(html)
+
+  const render = new MainRender(map, elements)
+  // const bottomUI = new BottomInterface(html, engine, events)
+  // const palette = new InstrumentsPalette(html, mode, events)
+
+  // const actions = new GameActions(mode, events, map, state, html, render)
+})
+
