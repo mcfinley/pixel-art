7d5e9dbc284afc9ca17bd41c54852d9e6df53a22
mcfinley <mcfinley@yandex.ru>
2020-07-05 09:35:12 +0300

    Change hero

--- src/modules/miscellaneous/hello-user/HelloHero/index.tsx
+++ src/modules/miscellaneous/hello-user/HelloHero/index.tsx
@@ -27,7 +27,7 @@ export default () => (
   <Container>
     <Heading>Hi, User 👋</Heading>
     <Paragraph>
-      Welcome to my demo 2D pixel art design tool.{' '}
+      Welcome to demo 2D pixel art design tool.{' '}
       <a href="#">Take a quick tour</a> or{' '}
       <a href="#">Visit project page</a>
     </Paragraph>
